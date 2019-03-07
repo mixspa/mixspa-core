ac6a358db3ad2dafc42083311a656bcb3ca9c853
Cao Xiaoqi <xqcao@thoughtworks.com>
2019-03-07 22:55:37 +0800

    upgrade version and update export file

--- src/Mixspa.js
+++ src/Mixspa.js
@@ -1,15 +1,17 @@
-import MixspaApp from '@mixspa/app';
-import MixspaEvent from '@mixspa/events';
-import MixspaLoader from '@mixspa/loader';
+import loadApp from './loadApp';
+import defineApp from './defineApp';
+import registerApp from './registerApp';
+import loadSimpleApp from './loadSimpleApp';
 import getAttributes from './getAttributes';
+import MixspaEvent from '@mixspa/events';
 
 const Mixspa = {
   on: MixspaEvent.on,
   emit: MixspaEvent.emit,
-  define: MixspaApp.define,
-  add: MixspaLoader.addAppInfo,
-  load: MixspaLoader.loadApp,
-  loadSimple: MixspaLoader.loadSimpleApp,
+  define: defineApp,
+  register: registerApp,
+  load: loadApp,
+  loadSimple: loadSimpleApp,
   getAttributes: getAttributes
 };
 
